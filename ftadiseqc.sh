b6e235cefa069b9584077b977c2e315ba613c7cb
devegoo <57900186+devegoo@users.noreply.github.com>
2022-04-29 16:13:21 +0200

    Add files via upload

--- ftadiseqc.sh
+++ ftadiseqc.sh
@@ -1,9 +1,6 @@
  #!/bin/bash
 # configure SATIP server name/IP address, here
-
 #SERVER="192.168.178.42"
-# cleanup old files
-
 SERVER="192.168.1.113"
 # T1 = tuner 1,T2 = tuner 2,T3 = tuner3,T4 = tuner 4
 # P1 = diseqc A,P2 = diseqc B,P3 = diseqc C,P4 = diseqc D
@@ -27,17 +24,13 @@ T4P1=4.8E
 T4P2=
 T4P3=
 T4P4=39.0E
-
-
-DIR=($T1P1 $T1P2 $T1P3 $T1P4 $T2P1 $T2P2 $T2P3 $T2P4 $T3P1 $T3P2 $T3P3 $T3P4 $T4P1 $T4P2 $T4P3 $T4P4 )
-
+DIR=($T1P1 $T1P2 $T1P3 $T1P4 $T2P1 $T2P2 $T2P3 $T2P4 $T3P1 $T3P2 $T3P3 $T3P4 $T4P1 $T4P2 $T4P3 $T4P4)
 OUTFILES=(*.php *.m3u /tmp/*.m3u)
 if [ -f $OUTFILES ]; then
     rm $OUTFILES
-
 fi
-rm -r $T1P1 $T1P2 $T1P3 $T1P4 $T2P1 $T2P2 $T2P3 $T2P4 $T3P1 $T3P2 $T3P3 $T3P4 $T4P1 $T4P2 $T4P3 $T4P4
-mkdir $T1P1 $T1P2 $T1P3 $T1P4 $T2P1 $T2P2 $T2P3 $T2P4 $T3P1 $T3P2 $T3P3 $T3P4 $T4P1 $T4P2 $T4P3 $T4P4
+rm -r "${DIR[@]}"
+mkdir "${DIR[@]}"
 	    echo '1' > $T1P1/N.txt
 	    echo '1' > $T1P2/N.txt
 	    echo '1' > $T1P3/N.txt
@@ -61,7 +54,6 @@ cd $i
 wget https://de.kingofsat.net/tv-$i-fta.php
 cd ..
 done
-
 for i in "${DIR[@]}";
 do
 cd $i
@@ -69,15 +61,12 @@ src=$(cat "N.txt")
 python ../getchannels.py $SERVER tv-$i-fta.php $src
 cd ..
 done
-
-
 mkdir FTA
 mkdir FTA/$(date +%F)
-sed 's/pol=V/pol=v/g' allChannels.m3u > /tmp/allChannels.m3u && sed 's/pol=H/pol=h/g'  /tmp/allChannels.m3u > FTA/$(date +%F)/ALL_FTA_$P1-$P2-$P3-$P4-$(date +%F)-$(date +%M).m3u
 for i in "${DIR[@]}";
 do
 sed 's/pol=V/pol=v/g' $i/tv-$i-fta.m3u > /tmp/tv-$i-fta.m3u && sed 's/pol=H/pol=h/g' /tmp/tv-$i-fta.m3u > FTA/$(date +%F)/TV-$i-FTA-$(date +%F)-$(date +%M).m3u
 done
-#rm tv-*-fta.m3u allChannels.m3u
-#rm -r 1/ 2/ 3/ 4/ *.m3u
 cat FTA/$(date +%F)/*.m3u > FTA/$(date +%F)/allChannels.m3u
+sed 's/pol=V/pol=v/g' FTA/$(date +%F)/allChannels.m3u > /tmp/allChannels.m3u && sed 's/pol=H/pol=h/g'  /tmp/allChannels.m3u > FTA/$(date +%F)/ALL_FTA_$P1-$P2-$P3-$P4-$(date +%F)-$(date +%M).m3u
+
