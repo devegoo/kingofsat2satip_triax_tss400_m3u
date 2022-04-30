bb32bdc0e5983984133e3a3a02b78724c42f3a60
devegoo <57900186+devegoo@users.noreply.github.com>
2022-04-30 22:08:46 +0200

    Add files via upload

--- AIO_do_sorta_ftadiseqc_setup_vlc_and_openwrt.sh
+++ AIO_do_sorta_ftadiseqc_setup_vlc_and_openwrt.sh
@@ -128,8 +128,11 @@ file=openwrt_www/*.m3u
 scp $file $place/
 newurl=satip-channellist-url=http://$IP/Master.m3u
 oldurl=satip-channellist-url=http://$IP/Master.m3u
+emptyurl=satip-channellist-url=
 sed 's/#satip-channelist=/satip-channelist=/g' ~/.config/vlc/vlcrc > /tmp/vlcrc && mv /tmp/vlcrc ~/.config/vlc/vlcrc
 sed 's/satip-channelist=/satip-channelist=CustomList/g' ~/.config/vlc/vlcrc > /tmp/vlcrc && mv /tmp/vlcrc ~/.config/vlc/vlcrc
 sed 's/#satip-channellist-url=/satip-channellist-url=/g' ~/.config/vlc/vlcrc > /tmp/vlcrc && mv /tmp/vlcrc ~/.config/vlc/vlcrc
+sed --expression "s@$emptyurl@$newurl@" ~/.config/vlc/vlcrc > /tmp/vlcrc && mv /tmp/vlcrc ~/.config/vlc/vlcrc
+sed --expression "s@$oldurl@$newurl@" ~/.config/vlc/vlcrc > /tmp/vlcrc && mv /tmp/vlcrc ~/.config/vlc/vlcrc
 sed --expression "s@$oldurl@$newurl@" ~/.config/vlc/vlcrc > /tmp/vlcrc && mv /tmp/vlcrc ~/.config/vlc/vlcrc
 
